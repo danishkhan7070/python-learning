b07270e13dc37e95df98a94267d7eb0d03172267
Danish Ali <danishalimarwat196@gmail.com>
2026-07-09 11:08:35 +0500

    Add Python variables examples
    Added examples of different variable types in Python.

--- 02_variables.py
+++ 02_variables.py
@@ -1,1 +1,45 @@
 
+#Sure! Here's a simple explanation about variables in Python:
+
+# Variables are used to store data in Python.
+# A variable is created when you assign a value using the (=) operator.
+
+# Integer variable
+x = 10
+
+# String variable
+y = "Hello"
+
+# Float variable
+z = 3.14
+
+# Boolean variables
+w = True
+m = False
+
+# List variable
+n = [1, 2, 3]
+
+# Dictionary variable
+o = {"name": "John", "age": 30}
+
+# Tuple variable
+p = (1, 2, 3)
+
+# Set variable
+q = {1, 2, 3}
+
+# NoneType variable
+r = None
+
+# Display output
+print("Integer:", x)
+print("String:", y)
+print("Float:", z)
+print("Boolean (True):", w)
+print("Boolean (False):", m)
+print("List:", n)
+print("Dictionary:", o)
+print("Tuple:", p)
+print("Set:", q)
+print("NoneType:", r)
